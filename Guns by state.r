37345033cc7d9c4a2df706c542c8fcc7980651e9
Steven Jordan <31150206+jordansteven1991@users.noreply.github.com>
2018-03-31 09:49:47 -0400

    Update Guns by state.r

--- Guns by state.r
+++ Guns by state.r
@@ -1,2 +1,7 @@
-gunData <- read.csv(file="c:\\Users\\The Ledgend\\Desktop\\gun stats.csv", header = TRUE, sep = ",")
-str(gunData)
+library(plotly)
+gunData <- read.csv(file="U:\\gun stats.csv", header = TRUE, sep = ",")
+
+plot_ly( x = gunData$State,
+         y = gunData$Firearm.Deaths,
+        name = "Firearm Death By State",
+        type = "bar")
